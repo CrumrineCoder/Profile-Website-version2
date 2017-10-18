f3bb7722630ecc5aca8c4a39a5fe95f7c231b2d0
CrumrineCoder <crumrinecoding@gmail.com>
2017-10-18 17:41:18 -0400

    Button now displays website info on click

--- portfolio javascript.js
+++ portfolio javascript.js
@@ -1,7 +1,7 @@
 var projects = [ 
 {
 	logo: "https://images.findagrave.com/photos/2015/20/481_1421880348.jpg", 
-	picture: "", 
+	picture: "https://images-na.ssl-images-amazon.com/images/M/MV5BNTYzMjc2Mjg4MF5BMl5BanBnXkFtZTcwODc1OTQwNw@@._V1_UX214_CR0,0,214,317_AL_.jpg", 
 	title: "Polling Website", 
 	shortDesc: "A fullstack website for creating and answering polls. There is user authentication and login and several options when making a poll such as optional Captcha, the ability to see a poll before voting, voting with multiple answers, writing one's own answer, or rescinding a vote. The user can also link a poll to her account if they're logged in, which allows them to edit the poll afterwards or delete it. There is also a search funtionality.",
 	tools: [], 
@@ -14,24 +14,57 @@ var projects = [
 
 ]
 
+var projectPic = document.getElementById("projectPic"); 
+var projectTitle = document.getElementById("projectTitle"); 
+var projectShortDescription = document.getElementById("projectShortDescription"); 
+var projectTools = document.getElementById("projectTools"); 
+var projectSkills = document.getElementById("projectSkills"); 
+var projectProductPaper = document.getElementById("projectProductPaper"); 
+var projectCode = document.getElementById("projectCode"); 
+var projectVisit = document.getElementById("projectVisit"); 
+
 var buttons = document.getElementById("projectSelectBoxes"); 
 window.onload=function(){
 	function changeProject(projectID){
-		console.log(projectID); 
+		projectTools.innerHTML  = ""; 
+		
+		projectSkills.innerHTML = ""; 
+		
+		projectPic.innerHTML = "<img src = '" + projects[projectID].picture + "' ></img> ";
+		
+		projectTitle.innerHTML = "<p> " + projects[projectID].title + "</p>"; 
+		
+		projectShortDescription.innerHTML = "<p> " + projects[projectID].shortDesc + "</p>";  
+		
+		projectTools.innerHTML += "<ul>";
+		for(var i=0; i<projects[projectID].tools.length; i++){
+			projectTools.innerHTML += "<li>" + projects[projectID].tools[i] + "</li>"; 
+		}
+		projectTools.innerHTML += "</ul>";
+		
+		projectSkills.innerHTML += "<ul>";
+		for(var i=0; i<projects[projectID].skills.length; i++){
+			projectSkills.innerHTML += "<li>" + projects[projectID].skills[i] + "</li>"; 
+		}
+		projectSkills.innerHTML += "</ul>";
+		
+		projectProductPaper.innerHTML = projects[projectID].productPaper; 
+		
+		projectCode.innerHTML = "<a href='" + projects[projectID].codeLink + "' class='button'>See Code</a>"
+		
+		projectVisit.innerHTML = "<a href='" + projects[projectID].websiteLink + "' class='button'>Visit Website</a>"
 		
 	}
 	for(var i=0; i<projects.length; i++){
 		var btn = document.createElement("BUTTON");
 		btn.type = "image"; 
-        console.log(projects[i].logo); 		
-		btn.innerHTML = '<img src="' + projects[i].logo + '" />'
-//		btn.id = "Button" + i; 
-		btn.class = "buttonSelect"; 
+		btn.innerHTML = '<img src="' + projects[i].logo + '" class="projectSelectImage" />'
+		btn.className = "projectSelectButton"; 
 		btn.onclick = function(id){ 
 			return function(){
 				changeProject(id);  
 			}; 
 		}(i);
-		buttons.append(btn);  
+		buttons.append(btn);   
 	}
 }
